a7c95219816d72db4877f5d60f269dc444696aa9
Hugh Vidler <hughvidler@hotmail.com>
2018-03-09 10:12:59 +1100

    Removed beforeEach in the demoPage.spec.js

--- client/spec/pages/demoPage.spec.js
+++ client/spec/pages/demoPage.spec.js
@@ -3,19 +3,10 @@ const DemoPage = require('../../src/js/pages/demoPage');
 let page;
 
 describe('The Demo Page', () => {
-  beforeEach(() => {
-    page = new DemoPage();
-  });
-
   describe('rendering', () => {
     it('should produce the correct HTML', () => {
-      console.log(page);
-      console.log(page.watchFace);
-
-      let element = document.getElementById('watch-face');
-      console.log(page.template());
+      page = new DemoPage();
       expect(page.template()).toContain('hello demoPage');
     });
   });
 });
-//    expect(element.innerText).toBe('hello demoPage');
