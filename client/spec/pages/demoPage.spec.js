2c321825da85799296600a7cb45ff0cf4dad9a26
Hugh Vidler <hughvidler@hotmail.com>
2018-03-09 15:09:57 +1100

    added before each to demoPage.spec.js

--- client/spec/pages/demoPage.spec.js
+++ client/spec/pages/demoPage.spec.js
@@ -3,10 +3,13 @@ const DemoPage = require('../../src/js/pages/demoPage');
 let page;
 
 describe('The Demo Page', () => {
+  beforeEach(() => {
+    page = new DemoPage();
+  });
+
   describe('rendering', () => {
     it('should produce the correct HTML', () => {
-      page = new DemoPage();
-      expect(page.template()).toContain('hello demoPage');
+      expect(page.template()).toContain('Hello, i am the demoPage');
     });
   });
 });
--- client/src/js/pages/demoPage.js
+++ client/src/js/pages/demoPage.js
@@ -2,7 +2,10 @@ const BasePage = require('./BasePage');
 
 class DemoPage extends BasePage {
   template() {
-    return `hello demoPage`;
+    return   `
+      <h1>DemoPage!</h1>
+      <p>Hello, i am the demoPage</p>
+    `;
   }
 }
 
